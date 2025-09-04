8bfbb89758dfdd371d6d6f809adf1214ff246f2b
ezequielMoyano <131402518+ezequielMoyano@users.noreply.github.com>
2025-09-04 15:15:10 -0300

    Delete React-03/Inicio-R1/src directory

--- React-03/Inicio-R1/src/components/Hola_mundo.jsx
+++
@@ -1,9 +0,0 @@
-import "../App.css"
-
-export default function Hola_mundo() {
-  let mensaje="Hola Mundo";
-    return (<div className="Contenedor">        
-        <h1 >{mensaje}</h1>
-    </div>
-    );
-}
